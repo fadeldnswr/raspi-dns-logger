69b8d2c8bb3ffa02f7009743f4dffad813b345ee
fadeldnswr <fadel.achmad@ui.ac.id>
2026-01-10 22:11:23 +0700

    Revised the constructor value

--- src/services/dns_logger.py
+++ src/services/dns_logger.py
@@ -3,7 +3,6 @@ DNS logger module to log DNS queries to Supabase.
 '''
 import os
 import sys
-import re
 import hashlib
 
 from src.exception.exception import CustomException
@@ -11,21 +10,16 @@ from src.logging.logging import logging
 
 from datetime import datetime 
 from dateutil import tz
+from dotenv import load_dotenv
 
 # Define Log path and Salt from environment variables
-LOG_PATH = os.environ.get("LOG_PATH").strip()
-SALT = os.environ.get("SALT").strip()
+load_dotenv()
+LOG_PATH = os.getenv("LOG_PATH")
+SALT = os.getenv("SALT")
 
 if not LOG_PATH or not SALT:
   logging.warning("LOG_PATH or SALT environment variables are not set.")
   raise ValueError("LOG_PATH and SALT must be set in environment variables.")
-
-# Define Query for regex pattern matching
-QUERY_RE = re.compile(
-  r'^(?P<mon>\w{3})\s+(?P<day>\d{1,2})\s+(?P<time>\d{2}:\d{2}:\d{2})\s+'
-  r'dnsmasq\[(?P<pid>\d+)\]:\s+query\[(?P<qtype>[A-Z0-9]+)\]\s+'
-  r'(?P<domain>\S+)\s+from\s+(?P<client_ip>\d{1,3}(?:\.\d{1,3}){3})'
-)
 
 # Define month mapping
 MONTHS = {
@@ -39,14 +33,10 @@ TIMEZONE = tz.tzlocal()
 # Create a class to handle DNS logging
 class DNSLogger:
   # Initialize with client IP
-  def __init__(self, client_ip: str, mon: str, day: str, hhmmss: str):
-    self.client_ip = client_ip
-    self.mon = mon
-    self.day = day
-    self.hhmmss = hhmmss
-  
+  def __init__(self):
+    pass
   # Define method to hash client IP with salt
-  def hash_client(self) -> str:
+  def hash_client(self, client_ip: str) -> str:
     '''
     Hash the client IP with the provided salt using SHA-256.\n
     params:
@@ -56,16 +46,17 @@ class DNSLogger:
     '''
     try:
       # Define the salted IP
-      logging.info(f"Hashing client IP: {self.client_ip} with salt.")
-      hashed_ip = hashlib.sha256(SALT + self.client_ip.encode("utf-8")).hexdigest()
+      logging.info(f"Hashing client IP: {client_ip} with salt.")
+      salted_ip = SALT.encode("utf-8")
+      hashed_ip = hashlib.sha256(salted_ip + client_ip.encode("utf-8")).hexdigest()
       
-      logging.info(f"Hashed IP: {hashed_ip}")
+      logging.info("IP has been hashed successfully.")
       return hashed_ip[:24]
     except Exception as e:
       raise CustomException(e, sys)
   
   # Define method to parse a timestamp
-  def parse_ts(self) -> datetime:
+  def parse_ts(self, mon: str, day: str, hhmmss: str) -> datetime:
     '''
     Parse timestamp from log entry components.\n
     params:
@@ -81,10 +72,10 @@ class DNSLogger:
       year = now.year
       
       # Define local datetime
-      logging.info(f"Parsing timestamp for {self.mon} {self.day} {self.hhmmss} {year}")
+      logging.info(f"Parsing timestamp for {mon} {day} {hhmmss} {year}")
       dt_local = datetime(
-        year, MONTHS[self.mon], int(self.day),
-        int(self.hhmmss[0:2]), int(self.hhmmss[3:5]), int(self.hhmmss[6:8]),
+        year, MONTHS[mon], int(day),
+        int(hhmmss[0:2]), int(hhmmss[3:5]), int(hhmmss[6:8]),
         tzinfo=TIMEZONE
       )
       
